f3795e0f480be48ad0708651746a8b3a4cd6f6a8
Mathis Detourbet <mathis.detourbet@gmail.com>
2019-12-11 11:48:37 +0100

    [+] add comments to fix warnings

--- app/application.go
+++ app/application.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	// load .env file
+	_ "github.com/MathisDetourbet/bookstore_users-api/config"
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,6 +10,7 @@ var (
 	router = gin.Default()
 )
 
+// StartApplication by mapping urls and run the router
 func StartApplication() {
 	mapUrls()
 	router.Run()
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/MathisDetourbet/bookstore_users-api/app"
-	_ "github.com/MathisDetourbet/bookstore_users-api/config"
 )
 
 func main() {
